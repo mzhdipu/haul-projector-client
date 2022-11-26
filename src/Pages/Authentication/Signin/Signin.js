eada454eaed568facd6123ebe2eb53a6ab337343
mzhdipu <mojammelhossaindipu@gmail.com>
2022-11-26 21:03:51 +0600

    created user login option

--- src/Pages/Authentication/Signin/Signin.js
+++ src/Pages/Authentication/Signin/Signin.js
@@ -1,8 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { GoogleAuthProvider } from "firebase/auth";
+import React, { useContext } from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../Root/Contexts/AuthProvider";
 
 const Signin = () => {
+    const {signInWithGoogle} = useContext(AuthContext)
+    const googleProvider = new GoogleAuthProvider()
+    const navigate = useNavigate();
+    const location = useLocation(); 
+    const from = location.state?.from?.pathname || '/'
 
+    const handleGoogle = () =>{
+      signInWithGoogle(googleProvider)
+      .then(result =>{
+        const user = result.user;
+        navigate(from, {replace: true})
+      })
+      .catch(error=>{
+        console.log(error) 
+      })
+    }
     const handleSubmit = (event) =>{
         event.preventDefault()
         const form = event.target
@@ -82,9 +100,10 @@ const Signin = () => {
                   </div>
 
                   <div>
-                    <button
-                      type="button"
-                      className="
+                  <button
+                    onClick={handleGoogle}
+                    type="button"
+                    className="
                                     relative
                                     inline-flex
                                     items-center
@@ -105,20 +124,44 @@ const Signin = () => {
                                     hover:text-black
                                     focus:text-black focus:outline-none
                                 "
-                    >
-                      <div className="absolute inset-y-0 left-0 p-4">
-                        <svg
-                          className="w-6 h-6 text-blue-600"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                        >
-                          <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                        </svg>
-                      </div>
-                      Sign up with Google
-                    </button>
-                  </div>
+                  >
+                    <div className="absolute inset-y-0 left-0 p-4">
+                      <FaGoogle/>
+                    </div>
+                    Sign up with Google
+                  </button>
+
+                  <button
+                    type="button"
+                    className="
+                                    relative
+                                    inline-flex
+                                    items-center
+                                    justify-center
+                                    w-full
+                                    px-4
+                                    py-4
+                                    mt-2
+                                    text-base
+                                    font-semibold
+                                    text-gray-700
+                                    transition-all
+                                    duration-200
+                                    bg-white
+                                    border-2 border-gray-200
+                                    rounded-md
+                                    hover:bg-gray-100
+                                    focus:bg-gray-100
+                                    hover:text-black
+                                    focus:text-black focus:outline-none
+                                "
+                  >
+                    <div className="absolute inset-y-0 left-0 p-4">
+                      <FaGithub/>
+                    </div>
+                    Sign up with Facebook
+                  </button>
+                </div>
                 </div>
               </form>
             </div>
